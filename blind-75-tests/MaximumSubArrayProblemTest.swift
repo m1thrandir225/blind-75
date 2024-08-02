5f0caacaca654900ed62c031ff142b099a7fdae9
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2024-08-02 13:49:21 +0200

    fix: actually adding the files...

--- blind-75-tests/MaximumSubArrayProblemTest.swift
+++ blind-75-tests/MaximumSubArrayProblemTest.swift
@@ -5,4 +5,31 @@
 //  Created by Sebastijan Zindl on 2.8.24.
 //
 
-import Foundation
+import XCTest
+
+
+class MaximumSubArrayProblemTest : XCTestCase {
+	func testOne() {
+		let nums = [-2,1,-3,4,-1,2,1,-5,4]
+		
+		let res = MaximumSubArray().solve(nums)
+		
+		XCTAssertEqual(res, 6)
+	}
+	
+	func testTwo() {
+		let nums = [1]
+		
+		let res = MaximumSubArray().solve(nums)
+		
+		XCTAssertEqual(res, 1)
+	}
+	
+	func testThree() {
+		let nums = [5,4,-1,7,8]
+		
+		let res = MaximumSubArray().solve(nums)
+		
+		XCTAssertEqual(res, 23)
+	}
+}
--- blind-75/MaximumSubArray.swift
+++ blind-75/MaximumSubArray.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+class MaximumSubArray {
+	func solve(_ nums: [Int]) -> Int {
+		var current: Int = 0;
+		var maximum: Float = -Float.infinity
+		
+		for index in 0..<nums.count {
+			current += nums[index]
+			maximum = max(maximum, Float(current))
+			if current < 0 {
+				current = 0
+			}
+		}
+		return Int(maximum);
+	}
+}
